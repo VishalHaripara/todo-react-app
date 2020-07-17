4b7c22fb1e6920804841f529c9110b78bc30a142
Vishal <@vishalharipara@gmail.com>
2020-07-17 09:50:38 +0530

    Updates

---
+++ precache-manifest.ae891d2af9f02c66f52b0f9e6ffba9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8708086f95c47b6f0c6c",
+    "url": "/todo-react-app/static/css/main.c7ddadd2.chunk.css"
+  },
+  {
+    "revision": "8708086f95c47b6f0c6c",
+    "url": "/todo-react-app/static/js/main.637c047a.chunk.js"
+  },
+  {
+    "revision": "5fd7fe22f3e7a4aea29a",
+    "url": "/todo-react-app/static/js/runtime~main.7e9341e7.js"
+  },
+  {
+    "revision": "95f1ab1bf8ac62f90998",
+    "url": "/todo-react-app/static/js/2.c33723ab.chunk.js"
+  },
+  {
+    "revision": "8c7158bce45b1c4f876c6bb82dc0929b",
+    "url": "/todo-react-app/static/media/logo.8c7158bc.png"
+  },
+  {
+    "revision": "6e1a81ae3bce2eda4c5f2aac50f81824",
+    "url": "/todo-react-app/index.html"
+  }
+];
